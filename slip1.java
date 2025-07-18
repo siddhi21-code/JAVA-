97c7f6f6347b51432599826dcf29f7c4ab3d6128
siddhi21-code <siddhishirke508@gmail.com>
2025-07-18 10:43:38 +0530

    ok

---
+++ slip1.java
@@ -0,0 +1,8 @@
+public  class slip1{
+    public static  void main(String[] args){
+        for(int i =65; i<90; i++){
+            System.out.print(Character.toString((char)i));
+        }
+    }
+
+}
